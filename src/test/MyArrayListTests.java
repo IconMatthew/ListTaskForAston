7c704ed3f7398a22b4d705ea437da98f7d03b56a
jadae <jadaevmatvei@gmail.com>
2024-02-10 18:29:54 +0300

    Added tests for add method

--- src/test/MyArrayListTests.java
+++ src/test/MyArrayListTests.java
@@ -84,4 +84,47 @@ public class MyArrayListTests {
 
         Assertions.assertEquals(expectedSortedList, sortedList);
     }
+
+    @Test
+    void testAdd(){
+        MyArrayList<Integer> integerMyArrayList = new MyArrayList<>(new Integer[]{12, 2, 4});
+        MyArrayList<Integer> expectedIntegerMyArrayList = new MyArrayList<>(new Integer[]{12, 2, 4, 123});
+
+        integerMyArrayList.add(123);
+
+        Assertions.assertEquals(expectedIntegerMyArrayList, integerMyArrayList);
+    }
+
+    @Test
+    void testAdd2Values(){
+        MyArrayList<Integer> integerMyArrayList = new MyArrayList<>(new Integer[]{12, 2, 4});
+        MyArrayList<Integer> expectedIntegerMyArrayList = new MyArrayList<>(new Integer[]{12, 2, 4, 123, -444});
+
+        integerMyArrayList.add(123);
+        integerMyArrayList.add(-444);
+
+        Assertions.assertEquals(expectedIntegerMyArrayList, integerMyArrayList);
+    }
+
+    @Test
+    void testAddToIndex(){
+        MyArrayList<Integer> integerMyArrayList = new MyArrayList<>(new Integer[]{12, 2, 4});
+        MyArrayList<Integer> expectedIntegerMyArrayList = new MyArrayList<>(new Integer[]{12, 124, 2, 4});
+
+        integerMyArrayList.add(1, 123);
+
+        Assertions.assertEquals(expectedIntegerMyArrayList, integerMyArrayList);
+    }
+
+    @Test
+    void testAddToIndex2Values(){
+        MyArrayList<Integer> integerMyArrayList = new MyArrayList<>(new Integer[]{12, 2, 4});
+        MyArrayList<Integer> expectedIntegerMyArrayList = new MyArrayList<>(new Integer[]{-444, 12, 2, 123, 4});
+
+        integerMyArrayList.add(2, 123);
+        integerMyArrayList.add(0, -444);
+
+        Assertions.assertEquals(expectedIntegerMyArrayList, integerMyArrayList);
+    }
+
 }
